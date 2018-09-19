83b27bb298d29c95b11adcb078095c9167942148
GGMartin97 <42999995+GGMartin97@users.noreply.github.com>
2018-09-19 22:27:56 +0800

    测试语音识别

--- test.py
+++ test.py
@@ -1,7 +1,46 @@
-from aip import AipSpeech
+import requests
 
-APP_ID='14240516'
-API_KEY='aMTBj0VxArQlLtNcbALiG7QV'
-SECRET_KEY='ybr2YatdUy5XEZp8mvYkwQH1ML1YY1eo'
+#获取tokent
+baidu_server = "https://openapi.baidu.com/oauth/2.0/token?"
+grant_type = "client_credentials"
+#API Key
+client_id = "aMTBj0VxArQlLtNcbALiG7QV"
+#Secret Key
+client_secret = "ybr2YatdUy5XEZp8mvYkwQH1ML1YY1eo" 
 
-client=AipSpeech(APP_ID,API_KEY,SECRET_KEY)
+#拼url
+url ="%sgrant_type=%s&client_id=%s&client_secret=%s"%(server,grant_type,client_id,client_secret)
+print(url)
+#获取token
+res = requests.post(url)
+print(res.text)
+token = json.loads(res.text)["access_token"]
+print(token)
+#24.b891f76f5d48c0b9587f72e43b726817.2592000.1524124117.282335-10958516
+#设置格式
+RATE = "16000"
+FORMAT = "pcm"
+CUID="raspberry"
+DEV_PID="1536"
+
+#以字节格式读取文件之后进行编码
+with open('16k.pcm', "rb") as f:
+    speech = base64.b64encode(f.read()).decode('utf8')
+size = os.path.getsize(r'C:\Users\Administrator\Desktop\16k-23850.amr')
+headers = { 'Content-Type' : 'application/json'} 
+url = "https://vop.baidu.com/server_api"
+data={
+
+        "format":FORMAT,
+        "rate":RATE,
+        "dev_pid":DEV_PID,
+        "speech":speech,
+        "cuid":CUID,
+        "len":size,
+        "channel":1,
+        "token":token,
+    }
+
+req = requests.post(url,json.dumps(data),headers)
+result = json.loads(req.text)
+print(result["result"][0])
